2da6a0d8fe69b0b2fe06730267a53b96fecb7936
Melokawka <84191586+Melokawka@users.noreply.github.com>
2022-01-11 22:37:00 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nrAlbumu="117794";
+            string sciezkaOdczytu = "zapis.txt";
+
+            using (var sw = new StreamWriter("zapis.txt", append: false))
+            {
+                sw.WriteLine(nrAlbumu);
+            }
+
+
+            using (var sr = new StreamReader(sciezkaOdczytu))
+            {
+                Console.Write(sr.ReadToEnd());
+            }
+
+            Console.WriteLine();
+            int przedOstatnia;
+            int liczbaKobiet=0;
+
+            using (var sr = new StreamReader("pesels.txt"))
+            {
+                var line = sr.ReadLine();
+                while (line != null) 
+                {
+                    przedOstatnia = line[line.Length - 2] - '0';
+
+                    Console.WriteLine(line);
+                    Console.WriteLine(przedOstatnia);
+
+                    if (przedOstatnia % 2 == 1)
+                    {
+                        Console.WriteLine("nieparzysta");
+                        liczbaKobiet++;
+                        Console.WriteLine(liczbaKobiet);
+                    }
+                    else
+                    {
+                        Console.WriteLine("parzysta");
+                        Console.WriteLine(liczbaKobiet);
+                    }
+
+
+                    line = sr.ReadLine();
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(liczbaKobiet);
+
+        }
+    }
+}
